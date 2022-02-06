7fef63f302fe7d5aee168c8f90a5a166dcd1012b
Thiago Jansen Sampaio Costa <thhiaggo1@hotmail.com>
2022-02-06 08:15:10 -0300

    novas config

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'absadboy',
+  host: 'ec2-18-116-73-46.us-east-2.compute.amazonaws.com',
+  username: 'webxr',
+  password: 'webxr',
   database: 'webxr',
   define: {
     timestamps: true,
